76da1ade514a6c7dc80e08012b6e6d452ae46740
meloko23 <meloko23@gmail.com>
2014-05-24 02:25:45 -0300

    Alterações de layout

--- SILCD/App_Start/BundleConfig.cs
+++ SILCD/App_Start/BundleConfig.cs
@@ -1,15 +1,13 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace SILCD
-{
-    public class BundleConfig
-    {
+namespace SILCD {
+    public class BundleConfig {
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
-        {
+        public static void RegisterBundles(BundleCollection bundles) {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
+
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -22,7 +20,7 @@ namespace SILCD
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
+         
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
